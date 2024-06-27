8c3bbc949287231302dbeca5b028780a38f0ed1a
vaibhavd2103 <bountydange@gmail.com>
2024-06-27 22:18:00 +0200

    trying individual data encryption and json storing

--- collection.py
+++ collection.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient, errors
 import base64
 import xmltodict
 import json
+from cryptography.fernet import Fernet
 
 # Function to scrape book data from a single page
 def scrape_page(url):
@@ -91,21 +92,45 @@ with open("books.xml", "rb") as file:
     
 # xml to json convertor
 json_data = xmltodict.parse(xml_data, dict_constructor=dict)
-required_json_data = json_data["books"]["book"]
+required_json_data = json.dumps(json_data)
+print(required_json_data)
+# required_json_data = json_data["books"]["book"]
 
 # Optional: Encode the XML data using base64
 # encoded_data = base64.b64encode(xml_data).decode('utf-8')
-encoded_data = json.dumps(required_json_data)
-print(encoded_data)
+# encoded_data = json.dumps(required_json_data)
+# print(encoded_data)
+
+# #this generates a key and opens a file 'key.key' and writes the key there
+# key = Fernet.generate_key()
+# with open('key.key','wb') as file:
+#     file.write(key)
+
+#this just opens your 'key.key' and assings the key stored there as 'key'
+with open('key.key','rb') as file:
+    key = file.read()
+
+# #this opens your json and reads its data into a new variable called 'data'
+# with open('filename.json','rb') as f:
+#     data = f.read()
+
+#this encrypts the data read from your json and stores it in 'encrypted'
+fernet = Fernet(key)
+encrypted = fernet.encrypt(required_json_data)
+print(encrypted)
+
+#this writes your new, encrypted data into a new JSON file
+with open('filename.json','wb') as f:
+    f.write(encrypted)
 
 # Prepare the document to be inserted
 document = {
-    "encoded_xml_data": encoded_data
+    # "encoded_xml_data": encoded_data
 }
 
 # Insert the document into the collection
 try:
-    collection.insert_one(document)
+    # collection.insert_one(document)
     print("Data has been successfully encoded (if applicable) and stored in MongoDB.")
 except errors.PyMongoError as e:
     print(f"An error occurred while inserting the document: {e}")
