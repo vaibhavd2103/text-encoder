40e827800bb8f25647e3703cc0bb1c9ccb064921
vaibhavd2103 <bountydange@gmail.com>
2024-07-07 20:44:17 +0200

    feat: folder change app.js

--- scraper_backend/index.js
+++ scraper_backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const bookRoutes = require("./book.routes");
+const bookRoutes = require("./src/book.routes");
 
 // Initialize Express app
 const app = express();
---
+++ scraper_frontend/constants/constants.ts
@@ -0,0 +1,1 @@
+export const backend_URL = "http://localhost:5000/api/books";
