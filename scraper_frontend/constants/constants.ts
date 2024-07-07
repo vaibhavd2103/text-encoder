bc1f8f56b321f634bb04d9672477e151eaddd4e0
vaibhavd2103 <bountydange@gmail.com>
2024-07-07 22:36:12 +0200

    patch: hosted backend url

--- scraper_frontend/constants/constants.ts
+++ scraper_frontend/constants/constants.ts
@@ -1,1 +1,2 @@
-export const backend_URL = "http://localhost:5000/api/books";
+// export const backend_URL = "http://localhost:5000/api/books";
+export const backend_URL = "https://text-encoder.onrender.com/api/books";
