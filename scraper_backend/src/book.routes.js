4f6d582c34788ce01ffb902316d72b760b43f457
19tanisha <tanishavanneri19@gmail.com>
2024-07-08 20:18:36 +0200

    feat: created get_all, get_by_id and add_book API's

--- scraper_backend/src/book.routes.js
+++ scraper_backend/src/book.routes.js
@@ -18,17 +18,33 @@ function decodeBase64(data) {
 
 // Get all books
 router.get("/", async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
   try {
-    const encodedData = await Book.find().limit(20);
+    // Convert `page` and `limit` to integers
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+
+    // Calculate the number of documents to skip
+    const skip = (pageNumber - 1) * limitNumber;
+    const encodedData = await Book.find().skip(skip).limit(limitNumber);
+    // Get the total count of documents
+    const totalDocuments = await Book.countDocuments();
     if (encodedData.length > 0) {
       const decodedData = encodedData.map((book) => ({
+        _id: book._id,
         title: decodeBase64(book.title),
         price: decodeBase64(book.price),
         rating: decodeBase64(book.rating),
         stock: decodeBase64(book.stock),
         image_url: decodeBase64(book.image_url),
       }));
-      res.send(decodedData);
+      res.json({
+        total: totalDocuments,
+        page: pageNumber,
+        limit: limitNumber,
+        totalPages: Math.ceil(totalDocuments / limitNumber),
+        decodedData,
+      });
     } else {
       res.send({
         message: "No data",
@@ -38,25 +54,26 @@ router.get("/", async (req, res) => {
     console.log(error);
   }
 });
+
 router.post("/addbook", async (req, res) => {
   const newBook = new Book({
-    title: "String",
-    price: "String",
-    stock: "String",
-    rating: "String",
-    description: "String",
-    tax: "String",
-    product_type: "String",
-    price_incl_tax: "String",
-    availability: "String",
-    num_reviews: "String",
-    image_url: "String",
+    title: req.body.title,
+    price: req.body.price,
+    stock: req.body.stock,
+    rating: req.body.rating,
+    description: req.body.description,
+    tax: req.body.tax,
+    product_type: req.body.product_type,
+    price_incl_tax: req.body.price_incl_tax,
+    availability: req.body.availability,
+    num_reviews: req.body.num_reviews,
+    image_url: req.body.image_url,
   });
 
   try {
     newBook.save().then((resp) => {
       res.send({
-        message: "altufaltu",
+        message: "Book added successfully",
       });
     });
   } catch (err) {
@@ -71,7 +88,24 @@ router.get("/:id", async (req, res) => {
   try {
     const book = await Book.findById(req.params.id);
     if (!book) return res.status(404).send("Book not found");
-    res.send(book);
+
+    // Decode Base64 fields
+    const decodedBook = {
+      title: decodeBase64(book.title),
+      price: decodeBase64(book.price),
+      rating: decodeBase64(book.rating),
+      stock: decodeBase64(book.stock),
+      image_url: decodeBase64(book.image_url),
+      // Include other fields as needed, decoded or as is
+      description: decodeBase64(book.description), // Assuming description is not encoded
+      tax: decodeBase64(book.tax),
+      product_type: decodeBase64(book.product_type),
+      price_incl_tax: decodeBase64(book.price_incl_tax),
+      availability: decodeBase64(book.availability),
+      num_reviews: decodeBase64(book.num_reviews),
+    };
+
+    res.send(decodedBook);
   } catch (err) {
     res.status(400).send("Invalid ID");
   }
